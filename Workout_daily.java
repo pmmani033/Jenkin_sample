05063d516cb088693f490dcb692e06bd032dfab6
Manikandan <98632393+pmmani033@users.noreply.github.com>
2025-02-20 11:25:49 +0530

    Update Workout_daily.java

--- Workout_daily.java
+++ Workout_daily.java
@@ -14,16 +14,16 @@ public class Workout_daily {
 	
 	
 
-	public static void main(String[] args) {
-
-		//System.setProperty("webdriver.chrome.driver", "C:\\c101\\chromedriver.exe");
+	@Test 
+       public void openAutomationTestingPage() {
         WebDriverManager.chromedriver().setup(); // Auto-manages ChromeDriver
-		WebDriver driver= new ChromeDriver();
-		driver.manage().window().maximize();
-		
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		
-		driver.get("https://demo.automationtesting.in/Register.html");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+        // Open the target URL
+        driver.get("https://demo.automationtesting.in/Register.html");
 		//driver.get("https://testautomationpractice.blogspot.com/");
 		//driver.get("https://demo.automationtesting.in/Slider.html");
 
@@ -32,28 +32,7 @@ public class Workout_daily {
 
 
 
-		/*
-		 * String random = RandomStringUtils.randomAlphanumeric(30); String strr=
-		 * random.replace("",""); String str1 = strr.substring(0, 6);
-		 * System.out.println(str1);
-		 * 
-		 * 
-		 * 
-		 * String str = "12345MANI";
-		 * 
-		 * // Using StringBuilder to store separated values StringBuilder alphabets =
-		 * new StringBuilder(); StringBuilder numbers = new StringBuilder();
-		 * 
-		 * for (char ch :str.toCharArray()) {
-		 * 
-		 * if(Character.isDigit(ch)) { numbers.append(ch); }else
-		 * if(Character.isLetter(ch)){ alphabets.append(ch); }
-		 * 
-		 * }
-		 * 
-		 * // Print results System.out.println("Alphabets: " + alphabets);
-		 * System.out.println("Numbers: " + numbers);
-		 */
+		
 
 
 
